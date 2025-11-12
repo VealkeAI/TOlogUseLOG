935281f8429aa687ebb102fcf2d38c06dce50aa7
WhiteChoke <denisduchenkoo@gmail.com>
2025-11-12 18:11:31 +0900

    raw project

---
+++ src/main/java/com/VealkeAI/TOlogUseLOG/TOlogUseLogApplication.java
@@ -0,0 +1,13 @@
+package com.VealkeAI.TOlogUseLOG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TOlogUseLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TOlogUseLogApplication.class, args);
+	}
+
+}
