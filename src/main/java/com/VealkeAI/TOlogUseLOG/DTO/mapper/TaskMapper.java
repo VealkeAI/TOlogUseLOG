56066192610843a37917f3d59a0c70d1ed99ebcd
WhiteChoke <denisduchenkoo@gmail.com>
2025-11-15 12:43:38 +0900

    created task mapper

---
+++ src/main/java/com/VealkeAI/TOlogUseLOG/DTO/mapper/TaskMapper.java
@@ -0,0 +1,46 @@
+package com.VealkeAI.TOlogUseLOG.DTO.mapper;
+
+import com.VealkeAI.TOlogUseLOG.DTO.TaskDTO;
+import com.VealkeAI.TOlogUseLOG.entity.TaskEntity;
+import com.VealkeAI.TOlogUseLOG.repository.TaskRepository;
+import com.VealkeAI.TOlogUseLOG.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class TaskMapper {
+
+    private final UserRepository userRepository;
+
+    public TaskDTO toDomain(TaskEntity entity) {
+        return new TaskDTO(
+                entity.getId(),
+                entity.getUser().getId(),
+                entity.getName(),
+                entity.getDescription(),
+                entity.getCreationTime(),
+                entity.getDeadline(),
+                entity.getPriority(),
+                entity.getState()
+        );
+    }
+
+    public TaskEntity toEntity(TaskDTO dto) {
+
+        var user = userRepository.findById(dto.id())
+                .orElseThrow(() -> new EntityNotFoundException("Not found user by id: " + dto.id()));
+
+        return new TaskEntity(
+                dto.id(),
+                user,
+                dto.name(),
+                dto.description(),
+                dto.creationTime(),
+                dto.deadline(),
+                dto.priority(),
+                dto.state()
+        );
+    }
+}
