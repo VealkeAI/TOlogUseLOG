073f40d1896d88972c6bc41b3b0736d742c88b69
WhiteChoke <denisduchenkoo@gmail.com>
2025-11-16 22:34:13 +0900

    small changes

--- src/main/java/com/VealkeAI/TOlogUseLOG/controller/TaskController.java
+++ src/main/java/com/VealkeAI/TOlogUseLOG/controller/TaskController.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("api/v1/task")
 @AllArgsConstructor
