7731f622046ac6e5d4bf8d016018ea6b5ecea58a
WhiteChoke <denisduchenkoo@gmail.com>
2025-11-19 17:54:35 +0900

    created scheduler service

---
+++ src/main/java/com/VealkeAI/TOlogUseLOG/service/scheduler/SchedulerService.java
@@ -0,0 +1,37 @@
+package com.VealkeAI.TOlogUseLOG.service;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import lombok.AllArgsConstructor;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class SchedulerService {
+
+    private final Logger logger = LoggerFactory.getLogger(SchedulerService.class);
+
+    private final Scheduler scheduler;
+
+    @PostConstruct
+    public void init() {
+        try {
+            scheduler.start();
+        } catch (SchedulerException e) {
+            logger.error(e.getMessage(), e);
+        }
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        try {
+            scheduler.shutdown();
+        } catch (SchedulerException e) {
+            logger.error(e.getMessage(), e);
+        }
+    }
+}
