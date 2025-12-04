a2a852132c43eb5708f53d9908c02ab7f6f06f3a
WhiteChoke <denisduchenkoo@gmail.com>
2025-12-04 19:57:02 +0900

    kafka configuration

--- src/main/java/com/VealkeAI/TOlogUseLOG/config/AppConfig.java
+++ src/main/java/com/VealkeAI/TOlogUseLOG/config/AppConfig.java
@@ -1,8 +1,19 @@
 package com.VealkeAI.TOlogUseLOG.config;
 
+import com.VealkeAI.TOlogUseLOG.service.schedulerJobs.SendMessageJob;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class AppConfig {
@@ -10,5 +21,30 @@ public class AppConfig {
     @Bean
     public RestTemplate restTemplate() {
         return  new RestTemplate();
+    }
+
+    /*
+     the serialization method is obsolete in kafka 4.0.0 and will be changed later
+     */
+    @Bean
+    public ProducerFactory<String, SendMessageJob> producerFactory(ObjectMapper objectMapper) {
+        Map<String, Object> configProperties = new HashMap<>();
+        configProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+
+        JsonSerializer<SendMessageJob> serializer = new JsonSerializer<>(objectMapper);
+        serializer.setAddTypeInfo(false);
+
+        return new DefaultKafkaProducerFactory<>(
+                configProperties,
+                new StringSerializer(),
+                serializer
+        );
+    }
+
+    @Bean
+    public KafkaTemplate<String, SendMessageJob> kafkaTemplate(
+            ProducerFactory<String, SendMessageJob> producerFactory
+    ) {
+        return new KafkaTemplate<>(producerFactory);
     }
 }
