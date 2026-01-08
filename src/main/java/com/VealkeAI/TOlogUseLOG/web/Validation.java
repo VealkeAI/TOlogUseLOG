d5ff26c0629dd9ce934ceb99d943ee4753822180
WhiteChoke <denisduchenkoo@gmail.com>
2026-01-08 16:45:56 +0900

    created validation class

--- src/main/java/com/VealkeAI/TOlogUseLOG/web/Validation.java
+++ src/main/java/com/VealkeAI/TOlogUseLOG/web/Validation.java
@@ -1,4 +1,16 @@
 package com.VealkeAI.TOlogUseLOG.web;
 
+import com.VealkeAI.TOlogUseLOG.DTO.taskDto.CreateTaskDTO;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+
+@Component
 public class Validation {
+
+    public void validateTask(CreateTaskDTO task, Instant currentTime) {
+        if(task.deadline() != null && task.deadline().isBefore(currentTime)) {
+            throw new IllegalArgumentException("Deadline cannot start in the past");
+        }
+    }
 }
